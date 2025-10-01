b573d49d2a832e7dd8c5d050a85f8d2eb4037e55
NathanSimon9 <nathansimon1234567@gmail.com>
2025-09-30 23:37:56 -0400

    pause

---
+++ affiche.gd
@@ -0,0 +1,20 @@
+class_name PanneauMessage extends Area2D
+
+@export var message := "Un message défaut."
+
+
+func _ready() -> void:
+	$Message.hide()
+
+
+func show_message(_body):
+	$Timer.start()
+	$Message.show()
+	$Message/fond/etiquette.text = message
+	await $Timer.timeout
+	$Message.hide()
+
+
+func hide_message(_body) -> void:
+	$Timer.stop()
+	$Message.hide()
--- scenes/camera_2d.gd
+++ scenes/camera_2d.gd
@@ -1,11 +1,31 @@
-extends Camera2D
+## Caméra que fait zoom in quand le joueur est proche d'un panneau.
+class_name ControleCamera extends Camera2D
+
+@export var zoom_closed := 1.8
+@export var zoom_open := 1.5
+@export var zoom_duration := 0.4
+
+var zoom_tween
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	var panneaux = get_tree().get_nodes_in_group("panneaux")
+	for p in panneaux:
+		p.body_entered.connect(zoom_in)
+		p.body_exited.connect(zoom_out)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func zoom_in(_body):
+	if zoom_tween:
+		zoom_tween.kill()
+	zoom_tween = self.create_tween()
+	zoom_tween.set_trans(Tween.TRANS_CUBIC)
+	zoom_tween.tween_property(self, "zoom", Vector2.ONE * zoom_closed, zoom_duration)
+
+
+func zoom_out(_body):
+	if zoom_tween:
+		zoom_tween.kill()
+	zoom_tween = self.create_tween()
+	zoom_tween.set_trans(Tween.TRANS_CUBIC)
+	zoom_tween.tween_property(self, "zoom", Vector2.ONE * zoom_open, zoom_duration)
